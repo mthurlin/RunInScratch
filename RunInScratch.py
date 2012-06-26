cc957861cc58cf419fac256f0cb50a1e7dfa5532
Stephen Horne <fatste@gmail.com>
2012-06-26 14:38:57 +0100

    Added a syntax definition for colour-coded output.

--- RunInScratch.py
+++ RunInScratch.py
@@ -1,26 +1,42 @@
 import sublime, sublime_plugin
 import commands, re
+import datetime
 
 class RunInScratchCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		current_file = self.view.file_name()
 		line1 = self.view.substr(self.view.line(0))
+		syntax = self.view.settings().get('syntax')
+		execute_string = self.get_execute_string(syntax, line1)
+		current_file = self.view.file_name()
 		self.current_file = current_file
-		file_name = current_file.split("/")[-1]
-		self.file_name = file_name
+		
 		window = sublime.active_window()
+
+		if current_file == None:
+			current_file = "None" + line1
+			file_name = self.view.substr(sublime.Region(0,self.view.size()))
+			tab_name = "untitled"
+			execute_string += get_execute_arg(syntax, line1)
+			file_directory = ""
+		else:
+			file_name = current_file.split("/")[-1]
+			tab_name = file_name
+			window.run_command("save")
+			file_directory = "/".join( current_file.split("/")[0:-1] )
+
+		self.tab_name = tab_name
+		self.file_name = file_name
 		self.window = window
 		
-		window.run_command("save")
-		
-		syntax = self.view.settings().get('syntax')
-		
-		file_directory = "/".join( current_file.split("/")[0:-1] )
-		
-		execute_string = self.get_execute_string(syntax, line1)
-		
 		cmd = "cd '%s'" % (file_directory) + "\n" + execute_string + " '%s'" % (file_name)
-		output = commands.getoutput(cmd)
+		
+		output = commands.getoutput(cmd).decode('utf-8')
+
+		if re.search("Ruby", str(syntax)):
+
+			header = execute_string.split('\n')[0] + ' -e ' + '\'puts "Ruby #{RUBY_VERSION} #{RUBY_PLATFORM} #{RUBY_RELEASE_DATE}\n\n"\''	
+			line1 = commands.getoutput(header).decode('utf-8')	
+			output = '\n'.join([line1,output])
 
 		view = self.get_results_window()
 
@@ -28,6 +44,13 @@ class RunInScratchCommand(sublime_plugin.TextCommand):
 		view.replace(edit, sublime.Region(0, view.size()), output)
 		view.sel().clear()
 		view.end_edit(edit)
+		
+	def get_execute_arg(self, syntax, line1):
+		return {
+			'applescript':' -e',
+			'python':' -c',
+			'ruby':' -e'
+		}[syntax]
 		
 	def get_execute_string(self, syntax, line1):
 		if re.match("#!", line1):
@@ -45,10 +68,12 @@ class RunInScratchCommand(sublime_plugin.TextCommand):
 			for view in window.views():
 				if view.settings().get('parent_file') == self.current_file:
 					return view
-				
+
 		window = sublime.active_window()
 		view = window.new_file()
+		view.set_syntax_file('/Users/fatboy/Library/Application Support/Sublime Text 2/Packages/RunInScratch/run_in_scratch.tmlanguage')
 		view.settings().set('parent_file', self.current_file)
+		view.settings().set('word_wrap', True)
 		window.run_command("set_layout",
 		      {
 		          "cols": [0.0, 0.65, 1.0],
@@ -56,10 +81,13 @@ class RunInScratchCommand(sublime_plugin.TextCommand):
 		          "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
 		      })
 		
+		
+		
+		view.set_name('Results of %s' % (self.tab_name))
+		
+		
 		window.run_command("move_to_group", {"group": 1})
 		window.run_command("focus_group", {"group": 0})
-		
-		view.set_name('Results of %s' % (self.file_name))
 		view.set_scratch(True)
 		
 		return view
